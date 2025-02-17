e6b3801b8cea1b58ca90c2740391d2354e6ec901
AyushKumarSingh866 <ayushkumarsingh866@gmail.com>
2025-02-17 22:12:36 +0530

    Init

--- Server/src/index.js
+++ Server/src/index.js
@@ -2,6 +2,9 @@ import express from "express";
 import { PORT } from "./constant.js";
 
 const app = express();
+app.get("/ayush", (req, res)=> {
+res.send("Ayush ")
+})
 app.listen(PORT, () => {
   console.log(`Server is running on dope mode on PORT ${PORT}`);
 });
