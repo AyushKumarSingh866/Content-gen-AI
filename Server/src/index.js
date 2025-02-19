feb3b06db9672cf60959dd94fa64cb76ca3fad88
AyushKumarSingh866 <ayushkumarsingh866@gmail.com>
2025-02-19 22:08:48 +0530

    Initialization of backend

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,10 +1,24 @@
-import express from "express";
-import { PORT } from "./constant.js";
+const express = require("express");
+const {connectMongoDb}= require('../connection')
+const{logReqRes}= require("../middlewares");
+const userRouter = require("../routes/user")
 
 const app = express();
-app.get("/ayush", (req, res)=> {
-res.send("Ayush ")
-})
+const PORT = 5006;
+
+connectMongoDb("mongodb://127.0.0.1:27017/app-1").then(()=>console.log("Mongodb Connected"));
+
+
+app.use(express.urlencoded({extended: false}));
+
+app.use(logReqRes("log.txt"));
+
+
+  
+
+
+app.use('/api/users', userRouter);
+
 app.listen(PORT, () => {
   console.log(`Server is running on dope mode on PORT ${PORT}`);
 });
